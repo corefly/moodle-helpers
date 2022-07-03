f763c424d64d81081b282226e4628c23f7da9963
Andrey Zhuk <jukandrey208@gmail.com>
2022-07-03 16:48:51 +0300

    Add cli command

---
+++ Corefly.Moodle.ConsoleApp/Commands/ConvertCommands/DocxToMoodleXmlConvertCommand.cs
@@ -0,0 +1,48 @@
+﻿using Corefly.Moodle.Converters.Converters;
+using Corefly.Moodle.Converters.Models;
+using Corefly.Moodle.Converters.Parsers;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Corefly.Moodle.ConsoleApp.Commands.ConvertCommands;
+
+public class DocxToMoodleXmlConvertCommand : AsyncCommand<DocxToMoodleXmlConvertCommand.Settings>
+{
+    public class Settings : ConvertSettings
+    {
+        public Settings(string sourceFile, string destinationFileLocation) : base(sourceFile, destinationFileLocation)
+        {
+        }
+    }
+
+    public override ValidationResult Validate(CommandContext context, Settings settings)
+    {
+        var unavailableSourceFile = !File.Exists(settings.SourceFile);
+
+        if (unavailableSourceFile)
+        {
+            return ValidationResult.Error($"Source file not found - {settings.SourceFile}");
+        }
+
+        return base.Validate(context, settings);
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        ICollection<Question> questions;
+
+        await using (var fileStream = File.OpenRead(settings.SourceFile))
+        {
+            questions = new DocxParser().Parse(fileStream);
+        }
+
+        if (questions.Any())
+        {
+            var xml = new MoodleXmlConverter().Convert(questions);
+            var path = Path.Combine(settings.DestinationFileLocation, "moodle.xml");
+            await File.WriteAllBytesAsync(path, xml);
+        }
+
+        return 0;
+    }
+}
--- Corefly.Moodle.ConsoleApp/Program.cs
+++ Corefly.Moodle.ConsoleApp/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace Corefly.Moodle.ConsoleApp;
+﻿using Corefly.Moodle.ConsoleApp.Commands.ConvertCommands;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Corefly.Moodle.ConsoleApp;
 
 internal class Program
 {
-    static void Main(string[] args)
+    private static int Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        AnsiConsole.Write(new FigletText("Moodle helpers").Color(Color.Plum1));
+
+        var app = new CommandApp();
+
+        app.Configure(config =>
+        {
+            config.AddBranch("convert", convert =>
+            {
+                convert.AddCommand<DocxToMoodleXmlConvertCommand>("docx-to-moodle-xml")
+                    .WithDescription("Convert docx to moodle xml");
+            });
+        });
+
+        return app.Run(args);
     }
 }
